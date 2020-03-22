66e6bac359607508dc17275192d305d072c8906e
silvus <silvus@silvus.fr>
2020-03-22 23:32:05 +0100

    Fix js warning on map init

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
-	var mymap = L.map('mapintervention').setView([48.453, 0.7024], 8);
+	// Leaflet Map init
+	var mapintervention = document.getElementById("mapintervention");
+	if (mapintervention) {
+		var mymap = L.map('mapintervention').setView([48.453, 0.7024], 8);
 
-	L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
-	}).addTo(mymap);
+		L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
+		}).addTo(mymap);
 
-	L.circle([48.453, 0.7024], {
-		color: 'green',
-		fillColor: '#03ab1a',
-		fillOpacity: 0.2,
-		radius: 60000
-	}).addTo(mymap);
+		L.circle([48.453, 0.7024], {
+			color: 'green',
+			fillColor: '#03ab1a',
+			fillOpacity: 0.2,
+			radius: 60000
+		}).addTo(mymap);
+	}
 });
 
 
